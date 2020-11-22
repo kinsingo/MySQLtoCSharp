468daa6156d93fe6af850991956b5c653484b3f3
kinsingo <jonghyun_shin@naver.com>
2020-11-22 14:43:33 +0900

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace MySqlCRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBookName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dgvBook = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_TableName = new System.Windows.Forms.TextBox();
+            this.button_Show_Tables = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBook)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Book Name :";
+            // 
+            // txtBookName
+            // 
+            this.txtBookName.Location = new System.Drawing.Point(136, 81);
+            this.txtBookName.Name = "txtBookName";
+            this.txtBookName.Size = new System.Drawing.Size(100, 21);
+            this.txtBookName.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 194);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dgvBook
+            // 
+            this.dgvBook.AllowUserToAddRows = false;
+            this.dgvBook.AllowUserToDeleteRows = false;
+            this.dgvBook.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBook.Location = new System.Drawing.Point(257, 67);
+            this.dgvBook.Name = "dgvBook";
+            this.dgvBook.RowTemplate.Height = 23;
+            this.dgvBook.Size = new System.Drawing.Size(537, 150);
+            this.dgvBook.TabIndex = 3;
+            this.dgvBook.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvBook_CellDoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Author : ";
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(136, 108);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(100, 21);
+            this.txtAuthor.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Description : ";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(136, 135);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(100, 53);
+            this.txtDescription.TabIndex = 1;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(95, 194);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(176, 194);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(613, 38);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(100, 21);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(14, 194);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(719, 37);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(257, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(187, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Create New Table";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(255, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Table Name :";
+            // 
+            // textBox_TableName
+            // 
+            this.textBox_TableName.Location = new System.Drawing.Point(344, 38);
+            this.textBox_TableName.Name = "textBox_TableName";
+            this.textBox_TableName.Size = new System.Drawing.Size(100, 21);
+            this.textBox_TableName.TabIndex = 6;
+            // 
+            // button_Show_Tables
+            // 
+            this.button_Show_Tables.Location = new System.Drawing.Point(613, 8);
+            this.button_Show_Tables.Name = "button_Show_Tables";
+            this.button_Show_Tables.Size = new System.Drawing.Size(181, 23);
+            this.button_Show_Tables.TabIndex = 7;
+            this.button_Show_Tables.Text = "Show Tables";
+            this.button_Show_Tables.UseVisualStyleBackColor = true;
+            this.button_Show_Tables.Click += new System.EventHandler(this.button_Show_Tables_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 224);
+            this.Controls.Add(this.button_Show_Tables);
+            this.Controls.Add(this.textBox_TableName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dgvBook);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.txtBookName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "MySqlCRUD APP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBook)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBookName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgvBook;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_TableName;
+        private System.Windows.Forms.Button button_Show_Tables;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace MySqlCRUD
+{
+    public partial class Form1 : Form
+    {
+        const string connectionString = "Server=localhost;Database=bookDB;Uid=root;Pwd=!jonghyun04;";
+        int bookID = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Clear();
+            DGVFill();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using(MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+                MySqlCommand mysqlcmd = new MySqlCommand("BookAddOrEdit", mysqlCon);
+                mysqlcmd.CommandType = CommandType.StoredProcedure;
+                mysqlcmd.Parameters.AddWithValue("_BookID", bookID);
+                mysqlcmd.Parameters.AddWithValue("_BookName", txtBookName.Text.Trim());
+                mysqlcmd.Parameters.AddWithValue("_Author", txtAuthor.Text.Trim());
+                mysqlcmd.Parameters.AddWithValue("_Description", txtDescription.Text.Trim());
+                mysqlcmd.ExecuteNonQuery();
+                MessageBox.Show("Submitted Successfully");
+                Clear();
+                DGVFill();
+            }
+        }
+
+        private void DGVFill()
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+                MySqlDataAdapter sqlDA = new MySqlDataAdapter("BookViewAll", mysqlCon);
+                sqlDA.SelectCommand.CommandType = CommandType.StoredProcedure;
+                DataTable datatable = new DataTable();
+                sqlDA.Fill(datatable);
+                dgvBook.DataSource = datatable;
+                //dgvBook.Columns[0].Visible = false;//BookID should not be shown.
+            }
+        }
+
+        private void Clear()
+        {
+            txtBookName.Clear();
+            txtAuthor.Clear();
+            txtDescription.Clear();
+            txtSearch.Clear();
+            bookID = 0;
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+        }
+
+        private void dgvBook_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvBook.CurrentRow.Index != -1)
+            {
+                bookID = Convert.ToInt32(dgvBook.CurrentRow.Cells[0].Value.ToString());
+                txtBookName.Text = dgvBook.CurrentRow.Cells[1].Value.ToString();
+                txtAuthor.Text = dgvBook.CurrentRow.Cells[2].Value.ToString();
+                txtDescription.Text = dgvBook.CurrentRow.Cells[3].Value.ToString();
+                btnSave.Text = "Update";
+                btnDelete.Enabled = true;
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+                MySqlDataAdapter sqlDA = new MySqlDataAdapter("booksearchByValues", mysqlCon);
+                sqlDA.SelectCommand.CommandType = CommandType.StoredProcedure;
+                sqlDA.SelectCommand.Parameters.AddWithValue("_SearchValue", txtSearch.Text.Trim());
+                DataTable datatable = new DataTable();
+                sqlDA.Fill(datatable);
+                dgvBook.DataSource = datatable;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sql_cmd = "CREATE TABLE " + textBox_TableName.Text + 
+               @"(BookID INT NOT NULL AUTO_INCREMENT,
+               BookName VARCHAR(45) NULL,
+               Author VARCHAR(45) NULL,
+              Description VARCHAR(250) NULL,
+              PRIMARY KEY(`BookID`))";
+
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+                MySqlCommand mysqlcmd = new MySqlCommand(sql_cmd.ToString(), mysqlCon);
+                mysqlcmd.CommandType = CommandType.Text;
+                mysqlcmd.ExecuteNonQuery();
+
+                MessageBox.Show(textBox_TableName.Text + " has been created");
+            }
+        }
+
+        private void button_Show_Tables_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+               
+                //List<string> QueryResult = new List<string>();
+                string ans = "";
+
+                MySqlCommand mysqlcmd = new MySqlCommand("show tables", mysqlCon);
+                MySqlDataReader reader = mysqlcmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    //QueryResult.Add(reader.GetString(0));
+                    ans += (reader.GetString(0) + "\n");
+
+                }
+                reader.Close();
+
+                MessageBox.Show(ans);
+            }
+
+
+             
+        }
+    }
+}
